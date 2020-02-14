d1eb79f4dcdb384d6fb0a29731aa7aaf15e3ff4c
WellsSA <wel.cavzod@gmail.com>
2020-02-14 20:34:36 -0300

    Envia HeaderComponent

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Content } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>Header</Content>
+    </Container>
+  );
+}
