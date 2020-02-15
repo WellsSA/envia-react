8421dd8b802457ad51e4a25b2d44f0e2aacaa6ff
WellsSA <wel.cavzod@gmail.com>
2020-02-14 21:07:04 -0300

    App main config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+// import { ToastContainer } from 'react-toastify';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import history from './services/history';
+// import { store, persistor } from './store';
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <div className="App">Here!</div>;
+  return (
+    <Router history={history}>
+      <Routes />
+      <GlobalStyle />
+    </Router>
+  );
 }
 
 export default App;
