cc6119cdc5e2b5f9ae23ea46e841b42ca2d802de
WellsSA <wel.cavzod@gmail.com>
2020-02-16 01:13:18 -0300

    Envia main theme definition

---
+++ src/styles/themes/envia.js
@@ -0,0 +1,12 @@
+export default {
+  title: 'envia',
+
+  colors: {
+    primary: '#558FC6',
+    highlight: '#33B5E5',
+
+    background: '#FFF',
+    backgroundHighlight: '#F2F2F2',
+    text: '#FFF',
+  },
+};
