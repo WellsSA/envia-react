888ac7359d70365112262b65223a7b68eff4edc4
WellsSA <wel.cavzod@gmail.com>
2020-02-18 23:22:36 -0300

    add memo to navlink

--- src/components/Sidebar/NavLink/index.jsx
+++ src/components/Sidebar/NavLink/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Container } from './styles';
 
-export default function NavLink({ to, label, icon: Icon, onClick }) {
+function NavLink({ to, label, icon: Icon, onClick }) {
   return (
     <Container>
       <Link to={to} onClick={onClick}>
@@ -26,3 +26,5 @@ NavLink.propTypes = {
 NavLink.defaultProps = {
   onClick: () => {},
 };
+
+export default memo(NavLink);
