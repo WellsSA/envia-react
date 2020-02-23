56cb55e17819acfd87a6392045817c4f1cad5205
WellsSA <wel.cavzod@gmail.com>
2020-02-22 23:11:10 -0300

    Added default index to export components in /components

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import EnviaDataTable from './EnviaDataTable';
+import Header from './Header';
+import NamedSection from './NamedSection';
+import Notifier from './Notifier';
+import OverlayHelper from './OverlayHelper';
+import Sidebar from './Sidebar';
+import Title from './Title';
+
+export {
+  EnviaDataTable,
+  Header,
+  NamedSection,
+  Notifier,
+  OverlayHelper,
+  Sidebar,
+  Title,
+};
