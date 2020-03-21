9b84057589f35a3098ba81baa1efc9a0834bdfb6
WellsSA <wel.cavzod@gmail.com>
2020-03-21 05:31:40 -0300

    fix: removing global styles Roboto import

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,8 +6,8 @@ import { getColor } from '../utils/themeHelper';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap');
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+  /* @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap'); */
 
   * {
     margin: 0;
