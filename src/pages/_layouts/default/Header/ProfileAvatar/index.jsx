5fafeedc969e4fb78f8fd95fc28d705721122a5c
WellsSA <wel.cavzod@gmail.com>
2020-06-20 21:10:03 -0300

    feature(): image upload option definition

--- src/pages/_layouts/default/Header/ProfileAvatar/index.jsx
+++ src/pages/_layouts/default/Header/ProfileAvatar/index.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
-
-import { Container, OptionBar } from './styles';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  Container,
+  Image,
+  OptionBar,
+  Option,
+  Background,
+  // FileLabel,
+} from './styles';
+import { signOut } from '~/store/modules/auth/actions';
 
 export default function ProfileAvatar() {
-  function toggleOptionBar() {
-    alert('here');
-  }
+  const dispatch = useDispatch();
+  const [optionBarOpen, setOptionBarOpen] = useState(true);
+  // const [file, setFile] = useState();
+
+  const toggleOptionBar = () => {
+    setOptionBarOpen(!optionBarOpen);
+  };
+
+  // const changeFile = e => {
+  //   const data = new FormData();
+  //   data.append('file', e.target.files[0]);
+
+  //   setFile(data);
+  // };
 
   return (
-    <Container onClick={toggleOptionBar}>
-      <img src="https://wellsadev.com/images/wells.jpg" alt="Envia" />
-      <OptionBar />
-    </Container>
+    <>
+      <Container onClick={toggleOptionBar}>
+        <Image src="https://wellsadev.com/images/well.jpg" alt="Envia" />
+        <OptionBar open={optionBarOpen}>
+          {/* <FileLabel htmlFor="avatar">
+            <Option>
+              {file ? 'Confirmar' : 'Alterar imagem'}
+              <input
+                type="file"
+                id="avatar"
+                accept="image/*"
+                onChange={changeFile}
+              />
+            </Option>
+          </FileLabel> */}
+          <Option onClick={() => dispatch(signOut())}>Sair</Option>
+        </OptionBar>
+      </Container>
+      <Background visible={optionBarOpen} onClick={toggleOptionBar} />
+    </>
   );
 }
--- src/pages/_layouts/default/Header/ProfileAvatar/styles.js
+++ src/pages/_layouts/default/Header/ProfileAvatar/styles.js
@@ -4,38 +4,74 @@ import { getColor } from '../../../../../utils/themeHelper';
 // import { fonts } from '../../../../../styles/scale';
 
 export const Container = styled.div`
-  /* width: 150px;
-  height: 150px; */
+  cursor: pointer;
   width: 120px;
   height: 120px;
   border-radius: 50%;
   position: relative;
-  cursor: pointer;
+  z-index: 200;
+`;
 
-  img {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    margin: 25px 0 0 5px;
-    /* width: 140%;
-    height: 140%; */
-    border: 7px solid
-      ${props => darken(0.2, getColor(props, 'backgroundHighlight'))};
-    border-radius: 50%;
-    background-attachment: fixed;
-    background-size: cover;
-    image-rendering: optimizeSpeed;
-    background-repeat: no-repeat;
+export const Image = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 25px 0 0 5px;
+  border: 7px solid
+    ${props => darken(0.2, getColor(props, 'backgroundHighlight'))};
+  border-radius: 50%;
+  background: ${props => getColor(props, 'primary')};
+  background-image: ${props => `url(${props.src})`};
+  image-rendering: optimizeSpeed;
+  background-repeat: no-repeat;
+  background-size: 100%;
+`;
+
+export const OptionBar = styled.ul`
+  visibility: ${props => (props.open ? 'visible' : 'hidden')};
+  position: absolute;
+  left: 90px;
+  top: 110px;
+  width: 160px;
+  background: ${props => getColor(props, 'background')};
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  color: #000;
+  border-radius: 4px;
+
+  display: flex;
+  flex-direction: column;
+  z-index: 201;
+`;
+
+export const Option = styled.li`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  color: ${props => getColor(props, 'text')};
+  border-bottom: thin solid ${props => getColor(props, 'backgroundHighlight')};
+  padding: 20px;
+  border-radius: 4px;
+
+  &:hover {
+    background: ${props => darken(0.04, getColor(props, 'background'))};
   }
 `;
 
-export const OptionBar = styled.div`
-  display: none;
-  position: absolute;
-  right: -30px;
-  bottom: -30px;
-  width: 100px;
-  height: 100px;
-  background: #ccc;
-  border-radius: 12px;
+export const Background = styled.div`
+  display: ${props => (props.visible ? 'block' : 'none')};
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 99;
+  background: rgba(0, 0, 0, 0.4);
+  z-index: 199;
+`;
+
+export const FileLabel = styled.label`
+  cursor: pointer;
+  input {
+    display: none;
+  }
 `;
