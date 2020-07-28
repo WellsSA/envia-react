2ae61abb7008e2a21c5e26f02a74bfe7ac457246
WellsSA <wel.cavzod@gmail.com>
2020-07-28 06:09:19 -0300

    feature(): Moving mobile warning to auth layout

--- src/pages/_layouts/auth/index.jsx
+++ src/pages/_layouts/auth/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Wrapper, Content } from './styles';
+import { Wrapper, Content, MobileWarning } from './styles';
+import { breakpoints } from '~/styles/scale';
 
 export default function AuthLayout({ children }) {
   return (
     <Wrapper>
-      <Content>{children}</Content>
+      {window.innerWidth > breakpoints.mobile ? (
+        <Content>{children}</Content>
+      ) : (
+        <MobileWarning>
+          <h1>
+            Ancioso pela versão mobile do ENVIA? <br />
+            Ela estará disponível em breve!
+          </h1>
+          <br />
+          <br />
+          <p>
+            Por hora, utilize um notebook ou desktop para obter a melhor
+            experiência possível :D
+          </p>
+        </MobileWarning>
+      )}
     </Wrapper>
   );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -123,3 +123,17 @@ export const Content = styled.div`
     }
   }
 `;
+
+export const MobileWarning = styled.div`
+  width: 100%;
+  height: 100%;
+  color: #fff;
+  background: linear-gradient(-90deg, #558fc6, #33b5e5);
+  display: flex;
+  flex-direction: column;
+  font-size: 16px;
+  padding: 40px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
